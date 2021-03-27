89f01a57829ffe88accc490dc14d0f7b5c4d223f
Marnus Coetzee <marnus@specno.com>
2021-03-27 14:56:49 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBIERBtxLAvQmmdaBQGHo6HAJW21WCe7Fg',
+    authDomain: 'fir-crud-af4db.firebaseapp.com',
+    projectId: 'fir-crud-af4db',
+    storageBucket: 'fir-crud-af4db.appspot.com',
+    messagingSenderId: '166391882359',
+    appId: '1:166391882359:web:a91c316d5d8b56fb921c72',
+    measurementId: 'G-GFW6B46XGM',
+  },
+};
